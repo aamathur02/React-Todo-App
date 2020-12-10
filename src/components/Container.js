6765737b8e25cf37e12c1426410762f36b96b9a1
Aayush Mathur <aamathur02@gmail.com>
2020-12-10 10:44:15 -0500

    app functions properly... now for some styling

--- src/components/Container.js
+++ src/components/Container.js
@@ -5,14 +5,32 @@ class Container extends React.Component {
     super(props);
     this.state = { taskList: [] };
     this.handleNewTask = this.handleNewTask.bind(this);
+    this.handleItemDelete = this.handleItemDelete.bind(this);
   }
 
   handleNewTask(item) {
-    this.setState({ taskList: this.state.taskList.concat(item) });
+    var newTask=  {
+      text: item,
+      key: Date.now()
+    };
+    this.setState({ taskList: this.state.taskList.concat(newTask) });
   }
 
+  
+  handleItemDelete(key) {
+    //alert(key);
+    var filteredItems = this.state.taskList.filter(function (task) {
+      return (task.key !== key);
+    });
+   
+    this.setState({
+      taskList: filteredItems
+    });
+  }
+  
+ 
   render() {
-    const listItems = this.state.taskList.map((task) => <li>{task}</li>);
+    const listItems = this.state.taskList.map((task) => <li onClick={() => this.handleItemDelete(task.key)}>{task.text}</li>);
     return (
       <div>
         <InputBox handleNewTask={this.handleNewTask} />
